bbb2386cca7fccf2c38ecec375c91ef35e1a7731
wuyeelok <wuyeelok@gmail.com>
2025-02-16 22:57:49 +0800

    Introducing Strings

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World! Part 4!");
+
+string firstFriend = "Tom";
+string secondFriend = "Jerry";
+Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
